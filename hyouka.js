974623fec13f9b2b92654e585d7d6f8a35dca638
Tetora248 <87902771+Tetora248@users.noreply.github.com>
2021-07-24 22:44:23 +0900

    Delete hyouka.js

--- hyouka.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#yes").on("click", function () {
-        $("#result").text("お役に立てて良かったです！");
-    });
-
-    $("#no").on("click", function () {
-        $("#result").text("お役に立てず申し訳ございません。");
-    });
-})
